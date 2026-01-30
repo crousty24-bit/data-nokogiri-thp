d23518d0694c295884b739d077ebe44b034c6667
crousty24-bit <allen.kch24@gmail.com>
2026-01-30 10:13:01 +0100

    feat: Add methode to improve code readibility

--- lib/data_cryptocurrencies.rb
+++ lib/data_cryptocurrencies.rb
@@ -1,20 +1,23 @@
 require 'nokogiri'
 require 'open-uri'
 
-coin_page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
+cryptos_url = "https://coinmarketcap.com/all/views/all/"
 
-all_cryptoS_row = coin_page.xpath('//tr[@class="cmc-table-row"]')
-all_cryptos_array = []
-
-all_cryptoS_row.each do |row|
-  cryptos_name = row.xpath('td[contains(@class, "symbol")]').text
-  
-  cryptos_price = row.xpath('td[contains(@class, "price")]').text.delete("$,").to_f
-  
-  all_cryptos_array<< {cryptos_name => cryptos_price}
+def cryptos_data(cryptos_list)
+  coin_page = Nokogiri::HTML(URI.open(cryptos_list))
+  all_cryptoS_row = coin_page.xpath('//tr[@class="cmc-table-row"]')
+  all_cryptos_array = []
+  all_cryptoS_row.each do |row|
+    cryptos_name = row.xpath('td[contains(@class, "symbol")]').text
+    
+    cryptos_price = row.xpath('td[contains(@class, "price")]').text.delete("$,").to_f
+    
+    all_cryptos_array<< {cryptos_name => cryptos_price}
+  end
+  return all_cryptos_array
 end
-puts "current array size: #{all_cryptos_array.length}"
-puts all_cryptos_array[0..19]
+  puts "all cryptos data: "
+  puts cryptos_data(cryptos_url)
 
 # array results limited to 20
 # if we wanted to extract all elements listed, we'll use gem 'Watir' instead
